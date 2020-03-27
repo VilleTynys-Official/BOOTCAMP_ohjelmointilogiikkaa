1218f3868cbf1c9fb5a7695d7d156a26cc6f8c21
Jane Doe <jane@example.com>
2020-03-27 11:18:03 +0200

    Program.cs lisätty pari muuttujaa testailua varten.

--- Program.cs
+++ Program.cs
@@ -4,8 +4,12 @@ namespace Ohjelmointilogiikkaa
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args)                 //main rutiini aloittaa ohjelman
         {
+            int i = 100;
+            int j = 200;
+            string s = "ABC";
+
             Console.WriteLine("Hello World!");
         }
     }
