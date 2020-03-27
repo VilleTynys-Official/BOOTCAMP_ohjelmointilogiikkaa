9607564089653e9e0af05e4ce789f1b36899acd4
Jane Doe <jane@example.com>
2020-03-27 13:28:57 +0200

    Arvaa luku poistettu ja harjoitus rakennettu suoraan Program.cs tiedostoon.

---
+++ ArvaaLuku.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Ohjelmointilogiikkaa
+
+   //HARJOITUS: Koodaa peli nimeltä Arvaa luku.Pelissä kone arpoo luvun 1-20,
+   //ja tämän jälkeen kysyy käyttäjältä arvausta luvusta.Jos arvattu luku on suurempi tai pienempi kuin oikea luku,
+   //näytetään tästä tieto käyttäjälle.Jos käyttäjä arvasi oikein, peli päättyy.Arvauskertoja on enintään 5 kpl.
+   
+{
+    public class ArvaaLuku
+    {
+        public ArvaaLuku()
+        {
+
+
+            //import random
+            var rand = new Random();
+
+            int rand_num = rand.Next();
+
+
+
+         
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,25 +6,58 @@ namespace Ohjelmointilogiikkaa
     {
         static void Main(string[] args)                 //main rutiini aloittaa ohjelman
         {
-            int i = 100;
-            int j = 200;
-            string s = "ABC";
 
-            if (i > 100)                //perus if
-            {
-                Console.WriteLine("i:n arvo on isompi kuin sata!");
-            }
-            else
-            {
-                Console.WriteLine("i:n arvo on pienempi kuin sata!");
-            }
+            //HARJOITUS: Koodaa peli nimeltä Arvaa luku.Pelissä kone arpoo luvun 1-20,
+            //ja tämän jälkeen kysyy käyttäjältä arvausta luvusta.Jos arvattu luku on suurempi tai pienempi kuin oikea luku,
+            //näytetään tästä tieto käyttäjälle.Jos käyttäjä arvasi oikein, peli päättyy.Arvauskertoja on enintään 5 kpl.
 
-            for (int laskuri = 0; laskuri < 10; laskuri++)
-            {
-                Console.WriteLine("moikka");
-            }
 
-         
+
+            //import random
+            var rand = new Random();
+            int rand_num = rand.Next(1,20);
+            Console.WriteLine(rand_num);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //int i = 100;
+            //int j = 200;
+            //string s = "ABC";
+
+            //if (i > 100)                //perus if
+            //{
+            //    Console.WriteLine("i:n arvo on isompi kuin sata!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("i:n arvo on pienempi kuin sata!");
+            //}
+
+            //for (int laskuri = 0; laskuri < 10; laskuri++)
+            //{
+            //    Console.WriteLine("moikka");
+            //}
+
+
+
         }
     }
 }
